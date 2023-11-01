8e1eb7087a13d5142db4b6e123918b1a37e9aaa8
horszczaruk-piotr <horszczaruk.piotr@gmail.com>
2023-11-01 18:55:38 +0100

    Initial commit

---
+++ src/main/java/com/example/premierleague/PremierLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.example.premierleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremierLeagueApplication.class, args);
+    }
+
+}
