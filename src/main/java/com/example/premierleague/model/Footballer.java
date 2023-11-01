956922acb2dc6270161e5344713a5efad67accf6
horszczaruk-piotr <horszczaruk.piotr@gmail.com>
2023-11-01 18:56:45 +0100

    Add Footballer model

---
+++ src/main/java/com/example/premierleague/model/Footballer.java
@@ -0,0 +1,13 @@
+package com.example.premierleague.model;
+
+public class Footballer {
+    String name;
+    String surname;
+    int age;
+    String country;
+    String position;
+    int number;
+    String team;
+
+
+}
